434e864adfbe9b38e00029ef7e1bb93e2cc67292
olo <olo@olek.impledo.com>
2012-09-21 00:32:29 +0200

    Adjust comments.

--- src/main/scala/bootstrap/liftweb/Start.scala
+++ src/main/scala/bootstrap/liftweb/Start.scala
@@ -34,21 +34,17 @@ object Start {
     val port = Props.getInt("jetty.emb.port", 9090)
     val server = new Server(port)
     val webctx = new WebAppContext
-    /* use embedded webapp dir as source of the web content -> webapp
-     * this is the dir within jar where we have put stuff with zip.
-     * it was in a directory created by package-war, in target (also
-     * named webapp), which was outside the jar. now, thanks to zip
-     * it's inside so we need to use method bellow to get to it.
-     * web.xml is in default location, of that embedded webapp dir,
-     * so we don't have do webctx.setDescriptor */
+    /* use embedded webapp dir as source of the web content
+     * web.xml is in default location, of embedded webapp dir,
+     * so don't need to adjust that */
     val webappDirInsideJar = webctx.getClass.getClassLoader.getResource("webapp").toExternalForm
     webctx.setWar(webappDirInsideJar)
 
     /* might use use external pre-existing webapp dir instead of referencing
      * the embedded webapp dir but it's not very useful. why would we put
-     * webapp inside if we end up using some other external directory. I put it
-     * for reference, may make sense under some circumstances.
-     * webctx.setResourceBase("webapp") */
+     * webapp inside jar if we end up using some other external webapp dir.
+     * I put it for reference, as it may make sense under some circumstances.
+     * webctx.setResourceBase("/path/to/existing/webapp-dir") */
 
     webctx.setContextPath("/")
     /* optionally extract embedded webapp to specific temporary location and serve
